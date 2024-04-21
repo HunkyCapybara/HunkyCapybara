df11dfe39e58fc3f620f6b5dc8570827812b7756
HunkyCapybara <162281341+HunkyCapybara@users.noreply.github.com>
2024-04-21 18:33:45 +0530

    Update task.sh

--- TerraformTask140424/task.sh
+++ TerraformTask140424/task.sh
@@ -16,4 +16,5 @@ sudo yum install maven -y
 
 # Tomcat Installation 
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.88/bin/apache-tomcat-9.0.88.tar.gz
-sudo tar -xvzf apache-tomcat-9.0.88.tar.gz 
+sudo tar -xvzf apache-tomcat-9.0.88.tar.gz
+ls
